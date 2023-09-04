2e9e07aa4b9354661ba6c8ca50e956bb2b12a5f6
Befend <18814382464@163.com>
2023-09-04 23:57:35 +0800

    feat(*): add vitest api about the function of the test.

--- codes/vitest-foundation/test.spec.js
+++ codes/vitest-foundation/test.spec.js
@@ -2,7 +2,7 @@ import { test, it, expect } from "vitest"
 import { add } from "./add"
 
 // test it
-// it -> BDD 行为驱动开发  TDD
+// it -> BDD 行为驱动开发  TDD 测试驱动学习
 // it should xxx xxx , Mocha Jasmine
 // test it Jest
 // vitest -> test it
@@ -16,12 +16,17 @@ it("should ", () => {
 	add()
 })
 
+// test.skip 跳过测试
+test.skip("should", () => {
+	add()
+})
+
 // test.todo("");
 
 // 功能
 // 1. xxx add
 // 2. xxx remove
-
-// test("add")
-// test("add reverse")
-// test.todo("remove")
+// test.todo 待开发事项，看得见的进度
+test("add")
+test("add reverse")
+test.todo("remove")
